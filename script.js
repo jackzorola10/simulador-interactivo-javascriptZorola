a093fe65be38e560f6a08f24bd12d8d8d6529bd2
Jack Zorolasl <jackzorola10@gmail.com>
2022-04-14 02:13:34 -0500

    Added the whole mid section about adding products, some refinement for usability needed but it seems the logic works wonderfully

--- script.js
+++ script.js
@@ -4,26 +4,73 @@
 // Finalmente le daremos una cantidad de puntos aleatorios al usuario, los cuales en su total del carrito le permitiremos que use para pagar menos si así lo desea.
 
 // CATALOGO DE PRODUCTOS #####
-let camiseta = 300;
-let gorra = 210;
-let vinil = 310;
-let paseAnual = 100;
-let greatestHits = 500;
-let chamarra = 700;
-
-// MEMBRESIA PREMIUM
-
+    let camiseta = 300;
+    let gorra = 210;
+    let vinil = 310;
+    let paseAnual = 100;
+    let greatestHits = 500;
+    let chamarra = 700;
+    let productsArray = [camiseta, gorra, vinil, paseAnual, greatestHits, chamarra]
 
 
+// INICIO DE ALGORITMO
+    alert("Hola! Bienvenido a la tienda de mercancía oficial del grupo musical Kay Vs the Moon!");
+    alert("Contamos con los siguientes productos: \n - Camiseta $" + productsArray[0] + "\n - Gorra $" + productsArray[1] + "\n - Disco Vinil $" + productsArray[2] + "\n - Pase Anual a conciertos $" + productsArray[3] + "\n - Disco de Greates Hits firmado $" + productsArray[4] + "\n - Chamarra roja de la banda $" + productsArray[5]);
 
-// ALGORTIMO
-alert("Hola! Bienvenido a la tienda de mercancía oficial del grupo musical Kay Vs the Moon!");
-alert("Contamos con los siguientes productos: \n - Camiseta $" + camiseta + "\n - Gorra $" + gorra + "\n - Disco Vinil $" + vinil + "\n - Pase Anual a conciertos $" + paseAnual + "\n - Disco de Greates Hits firmado $" + greatestHits + "\n - Chamarra roja de la banda $" + chamarra);
+// VALIDACIÓN INICIAL CODIGO SUPERFAN
+    let respuestaSuperFan = prompt("Antes de iniciar, queremos preguntarte, tienes un codigo Super Fan para mejores descuentos? (Si/No)");
+    while (respuestaSuperFan !== "Si" && respuestaSuperFan !== "No" ){
+        respuestaSuperFan = prompt("Por favor indique si tiene un codigo Super Fan con 'Si' o 'No'");  
+    }
+    console.log("respuestaSuperFan = " + respuestaSuperFan);
 
-let respuestaSuperFan = prompt("Antes de iniciar, queremos preguntarte, tienes un codigo Super Fan para mejores descuentos? (Si/No)");
+    let descuentoSuperFan
+    let codigoSuperFan
+    switch(respuestaSuperFan){
+        case "Si":
+            descuentoSuperFan = .3;
+            codigoSuperFan = prompt("Por favor ingrese su codigo SuperFan");
+            break;
+        
+        case "No":
+            descuentoSuperFan = 1;
+            break;
+    }
+    console.log("descuentoSuperFan = " + descuentoSuperFan);
+    console.log("codigoSuperFan = " + codigoSuperFan);
 
-if (respuestaSuperFan == "Si") {
-    let codigoSuperFan = prompt("Por favor ingresa tus 5 digitos de tu codigo super fan");
-}
 
+// PREGUNTA SOBRE ADICIÓN DE PRODUCTOS A CARRITO
+    
+    let carritoSubTotal = 0;
+    for (let i = 0; i < 1; i++) {
+        let productSelection = prompt(
+            "Selecciona de la lista de productos cual quisieras agregar a vuestro carrito! \n \n Escribe el numero que corresponda, por ejemplo: Pase Anual a conciertos = 4"
+             + "\n - Camiseta $" + (productsArray[0] - (productsArray[0]*descuentoSuperFan)) + " 1"
+              + "\n - Gorra $" + (productsArray[1] - (productsArray[1]*descuentoSuperFan)) + " 2"
+               + "\n - Disco Vinil $" + (productsArray[2] - (productsArray[2]*descuentoSuperFan)) + " 3"
+                + "\n - Pase Anual a conciertos $" + (productsArray[3] - (productsArray[3]*descuentoSuperFan)) + " 4"
+                 + "\n - Disco de Greates Hits firmado $" + (productsArray[4] - (productsArray[4]*descuentoSuperFan)) + " 5"
+                  + "\n - Chamarra roja de la banda $" + (productsArray[5] - (productsArray[5]*descuentoSuperFan)) + " 6");
 
+        console.log("El usuario escogió " + productSelection + " lo cual es igual a " + productsArray[productSelection]);
+
+        carritoSubTotal = carritoSubTotal + (productsArray[productSelection - 1] - (productsArray[productSelection - 1]*descuentoSuperFan));
+        console.log("carritoSubTotal = $" + carritoSubTotal);
+
+        // Falta validación en caso de que el usuario ingrese un dato no valido
+
+        let continuarPregunta = prompt("Os gustaria seguir comprando? (Si/No)");
+        while (continuarPregunta !== "Si" && continuarPregunta !== "No" ){
+            continuarPregunta = prompt("Por favor indique si vos queres seguir comprando con un 'Si' o un 'No'");  
+        }
+        console.log("continuarPregunta = " + continuarPregunta);
+
+        if (continuarPregunta == "Si") {
+            i--;
+        }
+        console.log("El valor de 'i' es = " + i)
+    }
+    
+
+// CALCULO SUBTOTAL Y OFRECIMIENTO DE PUNTOS (EN CASO DE SER SUPER FAN)
