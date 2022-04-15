aac9621fab80543f9c9e76527337e9b55a6c73b8
Jack Zorolasl <jackzorola10@gmail.com>
2022-04-14 22:36:25 -0500

    Added heavy documentation and some copy fixes

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 // Crearemos un carrito para un grupo musical, habrá un pequeño catalogo con multiples productos los cuales el usuario puede decidir si agregar al carrito o no.
 // Adicional crearemos el concepto de "Membresía Premium" lo cual le da acceso a distintos descuentos en cada producto. 
 // Finalmente le daremos una cantidad de puntos aleatorios al usuario, los cuales en su total del carrito le permitiremos que use para pagar menos si así lo desea.
+// Cada selector (Si/No) viene acompañado con un 'while' que asegura el input del usuario, este probablemente pueda cambiarse por una función, pero no me he dado el tiempo de hacerlo.
 
-// CATALOGO DE PRODUCTOS #####
+// CATALOGO DE PRODUCTOS 
+//Abajo encontrarás multiples variables con precios adjuntons a cada uno de ellos y un array con los nombres para selección del usuario.
     let camiseta = 300;
     let gorra = 210;
     let vinil = 310;
@@ -18,6 +20,8 @@
     alert("Contamos con los siguientes productos: \n - Camiseta $" + productsArray[0] + "\n - Gorra $" + productsArray[1] + "\n - Disco Vinil $" + productsArray[2] + "\n - Pase Anual a conciertos $" + productsArray[3] + "\n - Disco de Greates Hits firmado $" + productsArray[4] + "\n - Chamarra roja de la banda $" + productsArray[5]);
 
 // VALIDACIÓN INICIAL CODIGO SUPERFAN
+// El codigo SuperFan lo unico que hará es cambiar la variable 'descuentoSuperFan' para que pueda ser utilizada en darle un 30% de descuento a los precios mencionados arriba en el caso de que el usuario haya dicho que SI.
+
     let respuestaSuperFan = prompt("Antes de iniciar, queremos preguntarte, tienes un codigo Super Fan para mejores descuentos? (Si/No)");
     while (respuestaSuperFan !== "Si" && respuestaSuperFan !== "No" ){
         respuestaSuperFan = prompt("Por favor indique si tiene un codigo Super Fan con 'Si' o 'No'");  
@@ -41,7 +45,11 @@
 
 
 // PREGUNTA SOBRE ADICIÓN DE PRODUCTOS A CARRITO - MENU
-    
+// El codigo de esta parte esta dividido en 3 secciones: Selector, Validation de input del usuario, Verificación de repetición
+
+    // Selector:
+    // Establece las reglas del bucle 'for' y le da a escoger al usuario los productos a comprar, estos ademas varian en precio segun la respuesta sobre SuperFan. Finalmente altera el valor de carritoSubTotal que guarda la adición de los productos al carrito.
+    //      Encontrarás un secureCarritoSubTotal, el cual guarda el valor del subcarrito porque en caso de que el usuario ingrese un NaN o un numero distinto a las opciones del array, cambiaria el    carritoSubTotal y lo vuelve inutilizable. 
     let carritoSubTotal = 0;
     for (let i = 0; i < 1; i++) {
         let productSelection = prompt(
@@ -59,28 +67,22 @@
         carritoSubTotal = carritoSubTotal + (productsArray[productSelection - 1] - (productsArray[productSelection - 1]*descuentoSuperFan));
         console.log("carritoSubTotal = $" + carritoSubTotal);
 
-        // Falta validación en caso de que el usuario ingrese un dato no valido
-
-
-        
+        // Validation de input del usuario:
+        // Usa un switch para usar el default como cualquier otro escenario fuera de los deseados del usuario. En el caso de que este cause que carritoSubTotal sea NaN lo reestablece usando secureCarritoSubTotal y luego reinicia el bucle for.
         switch(productSelection){
             case "1":
                 break;
-            
             case "2":
                 break;
-
             case "3":
                 break;
-
             case "4":
                 break;  
-
             case "5":
                 break;
-
             case "6":
                 break;
+
             default:    
                 i--;
                 carritoSubTotal = secureCarritoSubTotal
@@ -89,7 +91,8 @@
         }
   
 
-
+        // Verificación de repetición: 
+        // Utiliza una validación sencilla que produce que el bucle se repita si el usuario escoge Si.
         let continuarPregunta = prompt("Os gustaria seguir añadiendo productos? (Si/No)");
         while (continuarPregunta !== "Si" && continuarPregunta !== "No" ){
             continuarPregunta = prompt("Por favor indique si vos queres seguir añadiendo productos con un 'Si' o un 'No'");  
@@ -105,10 +108,10 @@
     
 
 // CALCULO SUBTOTAL Y OFRECIMIENTO DE PUNTOS (EN CASO DE SER SUPER FAN)
-
+// Genera un numero random que luego multiplico para que cree un supuesto descuento equivalente a unos puntos acumulados del usuario para la simulación. El usuario puede luego decidir si aplicarlos o no a su Subtotal. El descuentoDePuntos es del 20% de los puntos, por arbitrariedad, solo porque noto que es una practica comun de retail que los puntos no representen 1:1 el descuento.
     let puntosAcumulados = Math.random() * 1000;
     let descuentoDePuntos = puntosAcumulados*.2;
-    let preguntaPuntos = prompt("Tu total hasta ahora es de: $" + carritoSubTotal + " os gustaria aplicar vuestros superFan points del id " + codigoSuperFan + "?" + "\n \nHasta ahora tienes " + puntosAcumulados + " puntos" + "\nSi los aplicas te ahorrarias: $" + descuentoDePuntos + "\n(Si/No)");
+    let preguntaPuntos = prompt("Tu total hasta ahora es de: $" + carritoSubTotal + " os gustaria aplicar vuestros puntos de descuento?" + "\n \nHasta ahora tienes " + puntosAcumulados + " puntos" + "\nSi los aplicas te ahorrarias: $" + descuentoDePuntos + "\n(Si/No)");
     while (preguntaPuntos !== "Si" && preguntaPuntos !== "No" ) {
         preguntaPuntos = prompt("Por favor indique si vos queres aplicar los puntos con un 'Si' o un 'No'");  
     }
@@ -129,7 +132,8 @@
     }
     console.log("El total de la orden es = " + total);
 
-    // JOKE
+// JOKE
+// Chascarrillo for the lolz.
 
     prompt("Por favor ponga su tarjeta de credito, fecha de expiración y CVV");
     alert("Es bromi :D - aquí termina el simulador");
